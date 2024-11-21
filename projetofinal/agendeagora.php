ab646e194a34caabe3c70b46226a0919660657a0
rafaellourenco11 <rafaeloliveiradelourenco@gmail.com>
2024-11-21 11:17:44 -0300

    Update agendeagora.php

--- projetofinal/agendeagora.php
+++ projetofinal/agendeagora.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body action="#" method="post">
-    <img src="img/logo.png">
-    
+    <h1>EM BREVE!</h1>
 </body>
 </html>
